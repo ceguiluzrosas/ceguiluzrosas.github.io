ff4802db3213ee3c5019d9d44a10ccb0375e2aad
Carlos Eguiluz Rosas <ceguiluzrosas@gmail.com>
2020-03-07 05:45:27 -0500

    idk 2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/www/project
-# python -m SimpleHTTPServer 8000 &
 service httpd start
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# kill -9 `ps -ef | grep SimpleHTTPServer | grep 8000 | awk '{print $2}'`
 isExistApp=`pgrep httpd`
 if [[ -n  $isExistApp ]]; then
     service httpd stop
